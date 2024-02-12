fb287ae4f6e17040349ee6af2457758de81a43e1
Erin Wise <erinwise88@gmail.com>
2024-02-12 15:22:45 -0500

    complete

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -8,12 +8,12 @@ class TestPlant:
 
     def test_plant_by_id_get_route(self):
         '''has a resource available at "/plants/<int:id>".'''
-        response = app.test_client().get('/plants/1')
+        response = app.test_client().get('/plants/2')
         assert(response.status_code == 200)
 
     def test_plant_by_id_get_route_returns_one_plant(self):
         '''returns JSON representing one Plant object at "/plants/<int:id>".'''
-        response = app.test_client().get('/plants/1')
+        response = app.test_client().get('/plants/2')
         data = json.loads(response.data.decode())
 
         assert(type(data) == dict)
@@ -23,13 +23,13 @@ class TestPlant:
     def test_plant_by_id_patch_route_updates_is_in_stock(self):
         '''returns JSON representing updated Plant object with "is_in_stock" = False at "/plants/<int:id>".'''
         with app.app_context():
-            plant_1 = Plant.query.filter_by(id=1).first()
+            plant_1 = Plant.query.filter_by(id=2).first()
             plant_1.is_in_stock = True
             db.session.add(plant_1)
             db.session.commit()
             
         response = app.test_client().patch(
-            '/plants/1',
+            '/plants/2',
             json = {
                 "is_in_stock": False,
             }
